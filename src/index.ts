e5cc8054dbc8071b3c48f4d4f59b828ffb7d5b25
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-08-18 23:10:41 -0300

    <feat> cors with domains

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,25 @@ import cors from "cors";
 const app = express();
 const PORT: number = 3000;
 
+const allowedOrigins = ['http://localhost:5173', 'http://192.168.100.153:5173', 'https://calculator-register.vercel.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Acceso no permitido por CORS'));
+    }
+  },
+  methods: ['POST'],
+}));
+
 app.listen(PORT, () => {
   console.log("Servidor levantado en el puerto: " + PORT);
 })
 
 app.use(express.json());
 app.use(express.text());
-app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hola mundo, primer servidor con typescript");
